842faec764b5469bffaf752d2a32708c85be9752
tranthanhtra13 <cheche130202@gmail.com>
2025-01-15 16:22:19 +0700

    PTPMQPTPMQL

--- PTPMQL/NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ PTPMQL/NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb8299ffae1d0b757a2f5f476451f2f6dc20d83f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea15cd6b1a6ddcba151d2c6832321bdad7aeea61")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
